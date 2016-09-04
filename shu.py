9b96a8d96c051e42347350f059efdd6c9131128b
cosformula <infkeko@live.cn>
2016-09-05 00:28:34 +0800

    *代码重构

--- shu.py
+++ shu.py
@@ -9,67 +9,51 @@ import re
 import requests
 app = Flask(__name__)
 
-
-def pelogin(user,pwd):
-    url='http://202.120.127.149:8989/spims/login.do?method=toLogin'
-    postData={'UNumber':user,'Upwd':pwd,'USnumber':u'上海大学'}
-    s = requests.Session()
-    try:
-        r = s.get('http://202.120.127.149:8989/spims/login/index.jsp',timeout=10)
-        r = s.post(url,data=postData,timeout=10)
-        r = s.get('http://202.120.127.149:8989/spims/exercise.do?method=seacheload',timeout=10)
-    except:
-        return False
-    else:
-        table_p=r'<table cellpadding="3" cellspacing="1" class="table_bg">([\s\S]*)<tr>\s+<td colspan="13">'
-        content=re.findall(table_p, r.text,re.S|re.M)
-        if(len(content)==0):
-            return False
-        return content[0]
-
-def finoldlogin(user,pwd):
-    url='http://finance.shu.edu.cn/Login.aspx?ReturnUrl=%2fTuition.aspx'
-    postData={'__EVENTTARGET':'',
+def general_login(site,user,pwd): #不使用验证码登录的网站的通用登录方法
+    if site == 'pe':#根据相应网站确定相应的postData
+        postData={'UNumber':user,'Upwd':pwd,'USnumber':u'上海大学'}
+    elif site == 'finold':
+        postData={'__EVENTTARGET':'',
     '__EVENTARGUMENT':'',
     '__VIEWSTATE':'/wEPDwUKLTM3NDg5MjY1NQ9kFgICAQ9kFgICAw9kFhACAQ8PFgIeBFRleHQFNw0KCQkJCQkJCQkJCTxiPueZu+W9leezu+e7nzwvYj7vvIjmnKznp5HmlLbotLnmn6Xor6LvvIlkZAIDDw8WAh4LTmF2aWdhdGVVcmwFNExvZ2luLmFzcHg/UmV0dXJuVXJsPSUyZlR1aXRpb24uYXNweCZFbXBsb3llZUxvZ2luPTFkZAITDw8WAh4HVmlzaWJsZWdkZAIUDw8WBB8ABcQBPGltZyBzcmM9cGljLzEuZ2lmIGJvcmRlcj0wPjxpbWcgc3JjPXBpYy8xLmdpZiBib3JkZXI9MD48aW1nIHNyYz1waWMvNS5naWYgYm9yZGVyPTA+PGltZyBzcmM9cGljLzUuZ2lmIGJvcmRlcj0wPjxpbWcgc3JjPXBpYy84LmdpZiBib3JkZXI9MD48aW1nIHNyYz1waWMvNy5naWYgYm9yZGVyPTA+PGltZyBzcmM9cGljLzQuZ2lmIGJvcmRlcj0wPh8CZ2RkAhYPDxYCHwJnZGQCFw8PFgQfAAWoATxpbWcgc3JjPXBpYy81LmdpZiBib3JkZXI9MD48aW1nIHNyYz1waWMvNS5naWYgYm9yZGVyPTA+PGltZyBzcmM9cGljLzUuZ2lmIGJvcmRlcj0wPjxpbWcgc3JjPXBpYy83LmdpZiBib3JkZXI9MD48aW1nIHNyYz1waWMvMi5naWYgYm9yZGVyPTA+PGltZyBzcmM9cGljLzQuZ2lmIGJvcmRlcj0wPh8CZ2RkAhkPDxYCHwJnZGQCGw8PFgIfAmdkZGTkUiQcxLjI7qbIyEZc3bENY5Ztjw==',
     '__EVENTVALIDATION':'/wEWBAKXm79JAtXh0pgPAqW0mtsGApOzq+INgXLh/Sj8FgD/gUxZsNSw4Q5MHNc=',
     'login1$txtName':user,
     'login1$txtPsw':pwd,
     'login1$Button1':''}
-    s = requests.Session()
-    r = s.post(url,data=postData,timeout=30)
-    string = re.search(r'<table id="Table1" cellspacing="0" cellpadding="0" width="759" align="center" border="0">([\s\S]*)</table>',r.text,flags=0).group(0)
-    string = re.sub(r'<span id="Tuition1_Label2">([\s\S]*)</select>', "", string)
-    string = re.sub(r"<font color='red'>([\s\S]*?)</font>", "", string)
-    return string
-
-
-def lehulogin(user,pwd):
-    postData={'username':user,'password':pwd,'url':'http://www.lehu.shu.edu.cn/'}
-    s = requests.Session()
+    elif site == 'lehu':
+        postData={'username':user,'password':pwd,'url':'http://www.lehu.shu.edu.cn/'}
+    else:
+        return False
+    
+    s = requests.session()#session初始化
+    #根据不同site发出不同的网络请求
     try:
-        r = s.post('http://passport.lehu.shu.edu.cn/ShowOrgUserInfo.aspx',data=postData,timeout=30)
-        r = s.get('http://card.lehu.shu.edu.cn/CardTradeDetail.aspx',timeout=30)
+        if site == 'pe':
+            r = s.get('http://202.120.127.149:8989/spims/login/index.jsp',timeout=10)
+            r = s.post('http://202.120.127.149:8989/spims/login.do?method=toLogin',data=postData,timeout=10)
+            r = s.get('http://202.120.127.149:8989/spims/exercise.do?method=seacheload',timeout=10)
+            string = r.text
+        elif site == 'finold':
+            r = s.post('http://finance.shu.edu.cn/Login.aspx?ReturnUrl=%2fTuition.aspx',data=postData,timeout=30)
+            string = r.text
+        elif site == 'lehu':
+            r = s.post('http://passport.lehu.shu.edu.cn/ShowOrgUserInfo.aspx',data=postData,timeout=30)
+            r = s.get('http://card.lehu.shu.edu.cn/CardTradeDetail.aspx',timeout=30)
+            string = r.text
     except:
         return False
     else:
-        table_p = r'<span id="ctl00_Contentplaceholder1_Label1">([\s\S]*)</form>'
-        content = re.findall(table_p,r.text,re.S|re.M)
-        if(len(content)==0):
-            return False
-        else:
-            return content[0]
+        #根据不同site进行不同的文本处理
+        if site == 'pe':
+            content=re.search(r'<table cellpadding="3" cellspacing="1" class="table_bg">([\s\S]*)<tr>\s+<td colspan="13">',string,flags=0).group(0)
+        elif site == 'finold':
+            content = re.search(r'<table id="Table1" cellspacing="0" cellpadding="0" width="759" align="center" border="0">([\s\S]*)</table>',string,flags=0).group(0)
+            content = re.sub(r'<span id="Tuition1_Label2">([\s\S]*)</select>', "", content)
+            content = re.sub(r"<font color='red'>([\s\S]*?)</font>", "", content)
+        elif site == 'lehu':
+            content = re.search(r'<span id="ctl00_Contentplaceholder1_Label1">([\s\S]*)</form>',string,flags=0).group(0)
+        return content
     return False
-
-@app.route('/')
-def index():
-    resp = make_response(render_template('index.html'))
-    return resp
-
-@app.route('/cal')
-def cal():
-    resp = make_response(render_template('cal.html'))
-    return resp
 
 def finquest():
     from PIL import Image
@@ -110,10 +94,10 @@ def phyquest():
     r = s.get('http://www.phylab.shu.edu.cn/openexp/index.php/Public/login/',timeout=10)
     phyhash = re.search(r'<input type="hidden" name="__hash__" value="([\s\S]*)" />',r.text,flags=0).group(1)
     r = s.get('http://www.phylab.shu.edu.cn/openexp/index.php/Public/verify/',timeout=10)
-    return BytesIO(r.content).getvalue().encode('base64') , s.cookies ,phyhash
+    return BytesIO(r.content).getvalue().encode('base64') , s.cookies , phyhash
 
-def phylogin(cookies,hash,user,pwd,check):
-    postData={'_hash_':hash,
+def phylogin(cookies,phyhash,user,pwd,check):
+    postData={'_hash_':phyhash,
     'account':user,
     'ajax':'1',
     'password':pwd,
@@ -130,51 +114,15 @@ def phylogin(cookies,hash,user,pwd,check):
     else:
         return string
 
- 
-@app.route('/phy',methods=['POST', 'GET'])
-def phy():
-    error = None
-    if request.method == 'POST':
-        usercookies = requests.cookies.RequestsCookieJar()
-        usercookies.set('PHPSESSID',request.cookies.get('PHPSESSID'))
-        r = phylogin(usercookies,request.cookies.get('_hash_'),request.form['username'],request.form['password'],request.form['check'])
-        if r != False:
-            resp = make_response(render_template('phy.html', r=r))
-            return resp
-        else:
-            error = u'登录失败！可能是账户密码错误或服务器宕机'
-            return render_template('index.html', error=error)
-    elif request.method == 'GET':
-        r,cookies,phyhash = phyquest()
-        error = u'请注意！若未修改过密码，初始密码为学号！！'
-        resp = make_response(render_template('loginp.html',r=r,error=error))
-        for cj in cookies:
-            resp.set_cookie(cj.name,cj.value)
-        resp.set_cookie('_hash_',phyhash)
-        return resp
-        
-    return render_template('index.html', error=error)
-@app.route('/fin',methods=['POST', 'GET'])
-def fin():
-    error = None
-    if request.method == 'POST':
-        usercookies = requests.cookies.RequestsCookieJar()
-        usercookies.set('ASP.NET_SessionId',request.cookies.get('ASP.NET_SessionId'),domain='shu.edu.cn')
-        usercookies.set('SFP_Verify_Cookie',request.cookies.get('SFP_Verify_Cookie'),domain='shu.edu.cn')
-        r = finlogin(usercookies,request.form['username'],request.form['password'],request.form['check'])
-        if r != False:
-            resp = make_response(render_template('fin.html', r=r))
-            return resp
-        else:
-            error = u'登录失败！可能是账户密码错误或服务器宕机'
-            return render_template('index.html', error=error)
-    elif request.method == 'GET':
-        r,cookies = finquest()
-        resp = make_response(render_template('loginc.html',r=r))
-        for cj in cookies:
-            resp.set_cookie(cj.name,cj.value)
-        return resp
-    return render_template('index.html', error=error)
+@app.route('/')
+def index():
+    resp = make_response(render_template('index.html'))
+    return resp
+
+@app.route('/cal')
+def cal():
+    resp = make_response(render_template('cal.html'))
+    return resp
 
 @app.route('/bus')
 def bus():
@@ -186,25 +134,51 @@ def map():
     resp = make_response(render_template('map.html'))
     return resp
 
-@app.route('/login/<site>', methods=['POST', 'GET'])
-def login(site):
+@app.route('/login/<check>/<site>', methods=['POST', 'GET'])
+#check变量代表此接口是否有验证码
+#site变量代表发生此接口是哪个接口
+def login(site,check):
     error = None
+    if check != 'vali':
+        check = None
     if request.method == 'POST':
-        if site == 'pe':
-            r = pelogin(request.form['username'],request.form['password'])
-        elif site == 'finold':
-            r = finoldlogin(request.form['username'],request.form['password'])
-        elif site == 'cardsurplus':
-            r = lehulogin(request.form['username'],request.form['password'])
+        if site == 'pe' or site =='finold' or site == 'lehu':
+            r = general_login(site,request.form['username'],request.form['password'])
+        elif site == 'phylab':
+            usercookies = requests.cookies.RequestsCookieJar()
+            usercookies.set('PHPSESSID',request.cookies.get('PHPSESSID'))
+            r = phylogin(usercookies,request.cookies.get('_hash_'),request.form['username'],request.form['password'],request.form['check'])
+        elif site == 'fin':
+            usercookies = requests.cookies.RequestsCookieJar()
+            usercookies.set('ASP.NET_SessionId',request.cookies.get('ASP.NET_SessionId'))
+            usercookies.set('SFP_Verify_Cookie',request.cookies.get('SFP_Verify_Cookie'))
+            r = finlogin(usercookies,request.form['username'],request.form['password'],request.form['check'])
         else:
-            r = False
+            return render_template('index.html', error=error)
         if r != False:
             return render_template(site+'.html', r=r)
         else:
             error = u'登录失败！可能是账户密码错误或服务器宕机'
-            return render_template('index.html', error=error)
-    return render_template('login.html', error=error)
-
+            return render_template('login.html', error=error , check=check)
+    elif request.method == 'GET':
+        if site == 'fin':
+            r,cookies = finquest()
+            error = u'请注意！若未修改过密码，初始密码为身份证后六位或学号后六位！！'
+            resp = make_response(render_template('login.html',r=r , error=error , check=check))
+            for cj in cookies:
+                resp.set_cookie(cj.name,cj.value)
+            return resp
+        elif site == 'phylab':
+            r,cookies,phyhash = phyquest()
+            error = u'请注意！若未修改过密码，初始密码为学号！！'
+            resp = make_response(render_template('login.html',r=r , error=error , check=check))
+            for cj in cookies:
+               resp.set_cookie(cj.name,cj.value)
+            resp.set_cookie('_hash_',phyhash)
+            return resp
+        else:
+            return render_template('login.html', error=error,check=check)
+    return render_template('index.html', error=error)
 
 if __name__ == '__main__':
     app.debug = True
