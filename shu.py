73f6481044d4e33b6790415258b5f5ddbbaed6ce
cosformula <infkeko@live.cn>
2016-12-14 00:11:04 +0800

    - 增加了课表查询 - 增加了配置文件 - 增加了关于我们 - 在可折叠列表处增加了可折叠的图标提示

--- shu.py
+++ shu.py
@@ -10,49 +10,10 @@ import requests
 import os
 import base64
 from sites import *
-app = Flask(__name__)
+from config import *
 
-# configuration
-app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'course.db'),
-    DEBUG=False,
-    SECRET_KEY='shuhelper',
-    USERNAME='admin',
-    PASSWORD='default'
-))
 cache = SimpleCache()
-app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-DAILY_WORDS = [u"不是世界变得无聊，而是你变成了无聊的人",
-u"DON'T PANIC",
-u"总而言之，不论发生了什么，只要还没死，人生就还没有结束－－人生仍会继续。也不会出现什么片尾曲或者STAFF字幕之类的东西",
-u"我们信守这些不言自明的真理：人人生而平等",
-u"人们不都是喜欢看着大家的笑容活下去吗？",
-u"在虚构的故事中寻找真实感的人一定脑袋有问题",
-u"猫经常揣手是因为他们知道这样很萌吗？",
-u"今天也肯定有某人，在世界的某个地方死去哦？所以穿丧服吧",
-u"诸葛亮知识广博，乃至在三国杀中需要三张将牌才能勉强概括他的技能。",
-u"英文咒语翻译成中文还有法术效果吗",
-u"莱特兄弟发明飞机以前，任何人如果想要飞到其他地方，都必须将 200 磅重的氦气吃下肚。",
-u"如果生活给了你土豆，就把他炸成土豆片 \(^o^)/ ",
-u"你今天真好看(*^_^*)",
-u"我们的成功是不应该通过企求别人的懈怠来达到的",
-u"本想给你做菜，可惜我没有锅。本想给你织围巾，可惜我没有线。本想给你写首诗，可惜我没有笔。",
-u"人生 ！ (/▽＼=)",
-u"自强不息",
-u"我们都是孤单的动物。我们把全部人生用来减轻孤独。",
-u"『这个世界上不存在才华，也不存在天赋，都是把事情一遍一遍重复做，下笨功夫，的来的成绩。』",
-u"『我岁数大了，就是想为国家多做贡献，其他无所挂念。』——钱伟长",
-u"每逢你想要批评任何人时，请记住，这个世界上所有的人，并不是个个都有过你所拥有的那些优越条件",
-u"以回赠她曾经送给我的那些可爱笑容",
-u"You will make a great discovery.",
-u"『这个世界上不存在才华，也不存在天赋，都是把事情一遍一遍重复做，下笨功夫，的来的成绩。』",
-u"前路未必顺利，但前路一定精彩",
-u"这颗星球上有无数微不足道的生命，但每一个生命都是可以歌唱的。",
-u"『这个世界上不存在才华，也不存在天赋，都是把事情一遍一遍重复做，下笨功夫，的来的成绩。』",
-u"年轻人是时常错过老人的",
-u"所有的爱都是为了相聚，只有父母与儿女的爱是为了别离",
-u"社会就是靠这种『异类』进步的呀",
-u"艺术，为了艺术的艺术，是这个世界上唯一重要的事情；艺术家凭一己之力赋予这荒谬的世界以意义。"]
+
 def randsession():
     import random
     import string
@@ -165,6 +126,9 @@ def login(site, check):
         s = general_login(s, site, usr, pwd, check_code, other)
         # return s
         if s != False:
+            # if site =='xkc':
+            #     flash(s)
+            #     return redirect(url_for('login',site=site,check=check))
             r = get_content(site, s)
             if r != False:
                 cache.set(session[site], s, timeout = 300)
@@ -178,7 +142,7 @@ def login(site, check):
                 flash(u'如果您的账号密码没填错的话，体育学院服务器又炸啦233...请过一段时间再试试')
             else:
                 flash(u'登录失败！可能是账户密码错误或服务器宕机')
-            return redirect(url_for('login',site=site,check=check))
+        return redirect(url_for('login',site=site,check=check))
     elif request.method == 'GET':
         if site in session and cache.get(session[site]) is not None and cache.get(session[site]+'islogin'):
             s = cache.get(session[site])
@@ -186,6 +150,8 @@ def login(site, check):
             return render_template(site+'.html', r=r)
         else:
             session[site] = randsession()
+            while cache.get(session[site]) is not None:
+                session[site] = randsession()
             s = requests.Session()
             if check == 'vali':
                 r, s, phy_hash = get_CAPTCHA(site, s)
--- sites.py
+++ sites.py
@@ -3,6 +3,7 @@
 import re
 import requests
 import base64
+from bs4 import BeautifulSoup
 def general_login(s, site, user, pwd, check = None, other = None): #网站的通用登录方法
     if site == 'pe':#根据相应网站确定相应的postData
         postData={'UNumber':user,
@@ -27,7 +28,11 @@ def general_login(s, site, user, pwd, check = None, other = None): #网站的通
         postData={'url':'',
         'txtUserNo':user,
         'txtPassword':pwd,
-        'txtValidateCode':check,}
+        'txtValidateCode':check}
+    elif site == 'xkc' or site =='xkl' :  # xkc for current semester, xkl for last semester
+        postData = {'txtUserName':user,
+        'txtPassword':pwd,
+        'txtValiCode':check}
     else:
         return False
     try:
@@ -49,12 +54,19 @@ def general_login(s, site, user, pwd, check = None, other = None): #网站的通
             r = s.post('http://cj.shu.edu.cn/',data = postData,timeout=60)
             r = s.get('http://cj.shu.edu.cn/Home/StudentIndex',timeout=10)
             success = r.text.find(u'首页') != -1
+        elif site == 'xkc':
+            r = s.post('http://xk.shu.edu.cn/',data = postData,timeout=60)
+            success = r.text.find(u'首页') != -1
+        elif site == 'xkl':
+            r = s.post('http://xk.shu.edu.cn:8080/',data = postData,timeout=60)
+            success = r.text.find(u'首页') != -1
     except:
         return False
     if success:
         return s
     else:
         return False
+        # return r.text
 def get_content(site, s, option=''):
     try:
         if site == 'pe':
@@ -96,9 +108,110 @@ def get_content(site, s, option=''):
             r = s.get('http://cj.shu.edu.cn/StudentPortal/ScoreQuery',timeout=20)
             string = re.search(r'<table class="tbllist">([\s\S]*?)</table>',r.text,flags=0).group(0)
             content = string
+        elif site == 'xkc': # 使用([\s\S]*)以贪婪匹配
+            r = s.get('http://xk.shu.edu.cn/StudentQuery/CtrlViewQueryCourseTable',timeout=20)
+            string = re.search(r'<table class="tbllist">([\s\S]*)</table>',r.text,flags=0).group(0)
+            content = string
+        elif site == 'xkl':
+            r = s.get('http://xk.shu.edu.cn:8080/StudentQuery/CtrlViewQueryCourseTable',timeout=20)
+            string = re.search(r'<table class="tbllist">([\s\S]*)</table>',r.text,flags=0).group(0)
+            content = string
         return content
     except:
         return False
+def get_json_from_course_table(content):
+    import json
+    table =re.search(r'<table class="tbllist">([\s\S]*?)</table>',content,flags=0).group(0)
+    soup = BeautifulSoup(table,"html.parser")
+    table = soup.find("table")
+    course_list = []
+    for row in table.findAll("tr")[2:]:
+        cells = row.findAll("td")[1:]
+        data = {
+        'courseno':cells[0].get_text(strip=True),
+        'coursename':cells[1].get_text(strip=True),
+        'teachno':cells[2].get_text(strip=True),
+        'teachname':cells[3].get_text(strip=True),
+        'credit':cells[4].get_text(strip=True),
+        'coursetime':cells[5].get_text(strip=True),
+        'courseplace':cells[6].get_text(strip=True),
+        'campus':cells[7].get_text(strip=True),
+        'qtime':cells[8].get_text(strip=True),
+        'qplace':cells[9].get_text(strip=True)
+        }
+        course_list.append(data)
+    return json.dumps(course_list)
+def get_binary_json_from_course_table(content,week):
+    import json
+    cn_num={
+        u'一':1,
+        u'二':2,
+        u'三':3,
+        u'四':4,
+        u'五':5,
+    }
+    time_table=[[],[],[],[],[]]
+    data={
+    'isempty':True,
+    'coursename':coursename
+    }
+    for days in time_table:
+        for i in range(1,14):
+            days.append(data)
+    table =re.search(r'<table class="tbllist">([\s\S]*?)</table>',content,flags=0).group(0)
+    soup = BeautifulSoup(table,"html.parser")
+    table = soup.find("table")
+    for row in table.findAll("tr")[2:]:
+        cells = row.findAll("td")[1:]
+        coursename=cells[1].get_text(strip=True)
+        coursetime=cells[5].get_text(strip=True)
+        while(True):
+            data={
+            'isempty':True,
+            'coursename':coursename
+            }
+            time = re.search(r'([\u4e00|\u4e8c|\u4e09|\u56db|\u4e94])([0-9]+)-([0-9]+)\s*(?:([\u5355|\u53cc|])|\((?:([0-9]+)-([0-9]+)\u5468)\)|\((?:([0-9]+),([0-9]+)\u5468)\))*',coursetime,flags=0)
+            coursetime = re.sub(r'([\u4e00|\u4e8c|\u4e09|\u56db|\u4e94])([0-9]+)-([0-9]+)\s*(?:([\u5355|\u53cc|])|\((?:([0-9]+)-([0-9]+)\u5468)\)|\((?:([0-9]+),([0-9]+)\u5468)\))*','',coursetime,1)
+            if time == None:
+                break
+            day = cn_num[time.group(1)]
+            start = int(time.group(2))
+            end = int(time.group(3))
+            if time.group(4)!=None:
+                if time.group(4) == u'单':
+                    if week%2==1:
+                        data['isempty']=False
+                else:
+                    if week%2==0:
+                        data['isempty']=False
+            elif time.group(5)!=None:
+                if week<=int(time.group(5)) and week>=int(time.group(4)):
+                    data['isempty']=False
+            elif time.group(7)!=None:
+                if week==int(time.group(7)) or week==int(time.group(8)):
+                    data['isempty']=False
+            for i in range(start,end):
+                time_table[day][i] = data
+    return json.dumps(time_table) 
+
+# def detect_conflict(data):
+#     time_list_raw = list_init()
+#     for time_list in data:
+#         for col in time_list:
+#             for blocks in col:
+#                 if blocks['isempty']==False:
+#                     time_list_raw[]
+
+def list_init():
+    list_raw = [[],[],[],[],[]]
+    data={
+    'isempty':True,
+    'coursename':coursename
+    }
+    for days in list_raw:
+        for i in range(1,14):
+            days.append(data)
+    return list_raw
 def get_CAPTCHA(site, s):
     from PIL import Image
     from io import BytesIO
@@ -110,8 +223,14 @@ def get_CAPTCHA(site, s):
         phyhash = re.search(r'<input type="hidden" name="__hash__" value="([\s\S]*)" />',r.text,flags=0).group(1)
         r = s.get('http://www.phylab.shu.edu.cn/openexp/index.php/Public/verify/',timeout=10,stream=True)
     elif site == 'cj':
-        r = s.get('http://cj.shu.edu.cn/',timeout=10,stream=True)
-        r = s.get('http://cj.shu.edu.cn/User/GetValidateCode?%20%20+%20GetTimestamp()',timeout=10,stream=True)
+        r = s.get('http://cj.shu.edu.cn/',timeout=20)
+        r = s.get('http://cj.shu.edu.cn/User/GetValidateCode?%20%20+%20GetTimestamp()',timeout=20,stream=True)
+    elif site == 'xkc':
+        r = s.get('http://xk.shu.edu.cn/',timeout=20)
+        r = s.get('http://xk.shu.edu.cn/Login/GetValidateCode?%20%20+%20GetTimestamp()',timeout=20,stream=True)
+    elif site == 'xkl':
+        r = s.get('http://xk.shu.edu.cn:8080',timeout=20)
+        r = s.get('http://xk.shu.edu.cn:8080/Login/GetValidateCode?%20%20+%20GetTimestamp()',timeout=20,stream=True)
 
     return base64.b64encode(r.raw.read()).decode('utf-8'), s, phyhash
 def nhce(user,pwd,cid):
@@ -130,6 +249,3 @@ def nhce(user,pwd,cid):
             return u'提交成功！请登录<a href="http://nhce.shu.edu.cn">nhce.shu.edu.cn</a>查看班级注册结果，如果不小心选错了可以在“班级注册”处注销班级重新选择'
         else:
             return False
-def test():
-    return 1
-
