05eecea5240ec4de32c4ef7a16fcb1a082bc1372
cosformula <infkeko@live.cn>
2016-12-18 20:30:47 +0800

    Use PyLint...

--- SHUhelper/SHUhelper/emptyroom.py
+++ SHUhelper/SHUhelper/emptyroom.py
@@ -1,28 +1,29 @@
+'''
+Find Empty Room
+'''
+import os
 import json
 import SHUhelper.schooltime
-import os
 from SHUhelper import app
-classroom_dict = {}
-json_file = open(os.path.join(app.root_path, 'classroomdata.json'))
-classroom_dict = json.load(json_file)
+CLASSROOM_DICT = {}
+JSON_FILE = open(os.path.join(app.root_path, 'classroomdata.json'))
+CLASSROOM_DICT = json.load(JSON_FILE)
 
-def is_room_empty(room,week,day,time):
-    return classroom_dict[room][week][day][time]==1
+def is_room_empty(room, week, day, time):
+    return CLASSROOM_DICT[room][week][day][time] == 1
 
 def get_emptyroom_now():
     week = SHUhelper.schooltime.this_week()
     day = SHUhelper.schooltime.this_day()
     time = SHUhelper.schooltime.this_class()
-    return get_emptyroom(week,day,time)
+    return get_emptyroom(week, day, time)
 
-def get_emptyroom(week,day,time):
-    emptyroom_list=[]
+def get_emptyroom(week, day, time):
+    emptyroom_list = []
     if time == 0 or day == 0 or day == 6:
         return emptyroom_list
-    for classroom in classroom_dict.keys():
-        if classroom_dict[classroom][week-1][day-1][time-1] == 1:
+    for classroom in CLASSROOM_DICT.keys():
+        if CLASSROOM_DICT[classroom][week-1][day-1][time-1] == 1:
             emptyroom_list.append(classroom)
     return sorted(emptyroom_list)
-
-# print(get_emptyroom_now())
 
