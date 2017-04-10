612d26a2cb4a153228e3e43b2d3a9e3ac0748522
cosformula <infkeko@live.cn>
2017-04-10 13:37:56 +0800

    adjust token generator, delete special char to avoid token expired

--- api.py
+++ api.py
@@ -44,7 +44,7 @@ def token():
     """
     import random
     import string
-    seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+=-"
+    seed = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
     sa = []
     for i in range(8):
         sa.append(random.choice(seed))
