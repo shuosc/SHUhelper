6669d1bc2989728460133dc940dc37130e03a55c
cosformula <m@cosformula.org>
2018-01-03 00:15:52 +0800

    * school bus timer

--- Web/src/components/IndexSchoolBus.vue
+++ Web/src/components/IndexSchoolBus.vue
@@ -21,17 +21,25 @@
             :options="stations")
       q-item(v-if="nearestDepartures.last||nearestDepartures.next")
         q-item-side(v-if="nearestDepartures.last")
-          | 上一趟 {{nearestDepartures.last}}
+          q-item-tile.text-center
+            | 上一趟 
+          q-item-tile.text-center
+            | {{nearestDepartures.last}}
         q-item-main(v-if="nearestDepartures.next")
-          q-item-tile
-            | 下一趟还有 {{nearestDepartures.next}}
+          q-item-tile.text-center
+            | 离下一趟还有 
+          q-item-tile.text-center
+            | {{nearestDepartures.wait}}分钟
         q-item-side(v-if="nearestDepartures.next")
-          | 下一趟
+          q-item-tile.text-center
+            | 下一趟
+          q-item-tile.text-center
+            | {{nearestDepartures.next}}
       q-card-separator
       q-card-actions
         q-collapsible.full-width(dense='', label='查看完整时刻表')
           q-list(no-border dense)
-            q-item(v-for="(departure,index) in departures")
+            q-item(v-for="(departure,index) in departures" :key="departure")
               q-item-side
                 | {{index+1}}
               q-item-main
@@ -46,43 +54,12 @@ function listToSelect(l) {
 }
 const BusSchedule = {
   嘉定校区南门: {
-    校本部: [
-      '09:00',
-      '10:00',
-      '11:00',
-      '12:00',
-      '13:00',
-      '14:00',
-      '15:00',
-      '17:00',
-      '21:00'
-    ],
+    校本部: ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '17:00', '21:00'],
     延长校区北门: ['18:00', '21:00', '途径校本部']
   },
   校本部: {
-    延长校区北门: [
-      '09:30',
-      '10:30',
-      '11:30',
-      '12:30',
-      '13:30',
-      '14:30',
-      '15:30',
-      '18:00',
-      '21:40'
-    ],
-    嘉定校区南门: [
-      '08:00',
-      '09:30',
-      '10:30',
-      '11:30',
-      '12:30',
-      '13:30',
-      '14:30',
-      '15:30',
-      '18:00',
-      '21:00'
-    ]
+    延长校区北门: ['09:30', '10:30', '11:30', '12:30', '13:30', '14:30', '15:30', '18:00', '21:40'],
+    嘉定校区南门: ['08:00', '09:30', '10:30', '11:30', '12:30', '13:30', '14:30', '15:30', '18:00', '21:00']
   },
   延长校区北门: {
     校本部: ['09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00'],
@@ -98,25 +75,49 @@ export default {
         from: '嘉定校区南门',
         to: '校本部'
       },
-      stations: listToSelect(['校本部', '延长校区北门', '嘉定校区南门'])
+      stations: listToSelect(['校本部', '延长校区北门', '嘉定校区南门']),
+      timerID: ''
     }
+  },
+  created() {
+    this.nearestDepartures()
+    this.timerID = setInterval(this.nearestDepartures, 30000)
+  },
+  watch: {
+    station: {
+      handler: function(val, oldval) {
+        this.nearestDepartures()
+      },
+      deep: true
+    }
+  },
+  beforeDestroy() {
+    clearInterval(this.timerID)
   },
   computed: {
     departures: function() {
       return this.busSchedule[this.station.from][this.station.to]
     },
     nearestDepartures: function() {
-      let time = new Date()
-      let minutes = time.getMinutes()
-      let hours = time.getHours()
+      let minutes
+      let hours
+      if (DEV) {
+        minutes = 40
+        hours = 14
+      } else {
+        let time = new Date()
+        minutes = time.getMinutes()
+        hours = time.getHours()
+      }
       for (let i = 0; i < this.departures.length; i++) {
         let departure = this.departures[i]
         let dHours = parseInt(departure.slice(0, 2))
-        let dMinutes = parseInt(departure.slice(2, 4))
-        if (dHours < hours || (dHours === hours && dMinutes < minutes)) {
+        let dMinutes = parseInt(departure.slice(3, 5))
+        if (hours < dHours || (dHours === hours && minutes < dMinutes)) {
           return {
             last: i > 0 ? this.departures[i - 1] : null,
-            next: this.departures[i]
+            next: this.departures[i],
+            wait: dHours * 60 + dMinutes - hours * 60 - minutes
           }
         }
       }
--- Web/src/router.js
+++ Web/src/router.js
@@ -79,7 +79,6 @@ export default new VueRouter({
     },
     {
       path: '/',
-      component: Index,
       redirect: '/index'
     },
     {
