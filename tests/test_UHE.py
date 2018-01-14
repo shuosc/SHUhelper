a2ba4e67172f027352fad206c4c779263af99d3a
cosformula <m@cosformula.org>
2018-01-15 00:01:43 +0800

    * pass test

--- tests/test_UHE.py
+++ tests/test_UHE.py
@@ -2,7 +2,7 @@ from UHE.app import create_app
 from UHE.user.api import login
 from UHE.calendar.api import now
 import flask_login
-app = create_app()
+# app = create_app()
 
 def test_app_start():
     return True
