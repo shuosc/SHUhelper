2f98a7a3f90fff9e419397920a969e14260c874a
longfangsong <longfangsong@icloud.com>
2019-01-02 16:56:38 +0800

    fix(backend): 修复mongodb名称冲突

--- backend/src/infrastructure/mongo.ts
+++ backend/src/infrastructure/mongo.ts
@@ -3,6 +3,6 @@ import {Db, MongoClient} from "mongodb";
 export let mongo: Db = null;
 
 export async function initDB() {
-    let client = await MongoClient.connect("mongo://localhost:4004", {useNewUrlParser: true});
+    let client = await MongoClient.connect("mongodb://localhost:4004", {useNewUrlParser: true});
     mongo = client.db('default');
 }
